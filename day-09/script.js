15470fc07b5b0e1869f2433db87dea5838aa12c8
Dari Alves <darieltondf@gmail.com>
2021-01-25 21:29:45 -0300

    sound board

---
+++ day-09/script.js
@@ -0,0 +1,25 @@
+const sounds = ['choose', 'brutality', 'finish', 'fatality', 'excellent', 'get-over-here', 'toasty', 'fight'];
+
+sounds.forEach(sound => {
+    const btn = document.createElement('button');
+    btn.classList.add('btn');
+
+    btn.innerHTML = sound;
+
+    btn.addEventListener('click', () => {
+        stopSongs()
+
+        document.getElementById(sound).play()
+    })
+
+    document.getElementById('buttons').appendChild(btn);
+});
+
+function stopSongs() {
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound);
+
+        song.pause();
+        song.currentTime = 0;
+    })
+}
